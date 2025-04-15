ad0d20b2b538825e40053fa1312aab7e9bfbb964
rubygorilla <productstrategywithaakash@gmail.com>
2025-04-15 21:21:49 +0530

    Fixed navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import StoreProfile from './pages/storeProfile'; // ⬅️ Add this import
-import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <div className="container mt-4">
         <Routes>
           <Route path="/" element={<Home />} />
